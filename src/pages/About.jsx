9911dc89673f686ebebaa86cbc35db823b7df1ae
benxur06 <axmedovbenxur@gmail.com>
2024-06-05 16:49:21 +0500

    about js ozgardi

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,6 +5,7 @@ const About = () => {
     <div>
       <h2 className="text-2xl font-bold m-12">About page</h2>
       <Link to={"/about/info"}>go to info page</Link>
+      <div>test test test</div>
     </div>
   );
 };
